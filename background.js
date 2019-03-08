252d90ebf07364c0ce14174be6391773488a0b7d
pjd <rovyko@protonmail.com>
2019-03-08 18:48:34 -0500

    add ability to open sidebar on shortcut
    shortcut is CTR+SHIFT+Y
    toggle (close while open not working)

--- background.js
+++ background.js
@@ -3,3 +3,14 @@ function handleClick() {
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
+
+browser.commands.onCommand.addListener(function (command) {
+  if (command == "toggle-feature") {
+    browser.sidebarAction.open();
+    browser.sidebarAction.isOpen({}).then(isOpen => {
+      if (isOpen) browser.sidebarAction.close();
+    });
+    console.log("toggling the feature!");
+  }
+
+});
